2a829e5134ee0362027786f78e4f7ec7d51dc9b7
Hiep Phuc Thai <46857957+tphiepbk@users.noreply.github.com>
2026-08-18 07:37:12 +0000

    Use free command to get memory status quickly
    Signed-off-by: Hiep Phuc Thai <46857957+tphiepbk@users.noreply.github.com>

--- utils/get_mem_stat.sh
+++ utils/get_mem_stat.sh
@@ -1,75 +1,47 @@
 #!/bin/bash
 
-TOTAL_MEM_MB=
-TOTAL_MEM_MB=$(top -bn 1 | grep "MiB Mem" | cut -d "," -f 1 | sed 's/[a-zA-Z: ]//g; s/+//g')
+function _parse_free_command () {
+    # TYPE should be: total, used, available
+    local TYPE="$1"
+    # UNIT should be: b, kb, mb and gb
+    local UNIT="-${2:0:1}"
 
-TOTAL_MEM_GB=
-TOTAL_MEM_GB=$(awk "BEGIN {printf \"%.1f\", $TOTAL_MEM_MB/1024}")
+    case "$TYPE" in
+        "total")
+            TYPE=2
+        ;;
+        "used")
+            TYPE=3
+        ;;
+        "available")
+            TYPE=7
+        ;;
+    esac
 
-function _get_used_mem () {
-    local USED_MEM_MB=
-    USED_MEM_MB=$(top -bn 1 | grep "MiB Mem" | cut -d "," -f 3 | sed 's/[a-zA-Z: ]//g; s/+//g')
-    local USED_MEM_GB=
-    USED_MEM_GB=$(awk "BEGIN {printf \"%.1f\", $USED_MEM_MB/1024}")
-
-    local PERCENT_USED=
-
-    if [[ "$1" == "mb" ]]; then
-        PERCENT_USED=$(awk "BEGIN {printf \"%.0f\", $USED_MEM_MB / $TOTAL_MEM_MB * 100}")
-        echo "${USED_MEM_MB}M/${TOTAL_MEM_MB}M ($PERCENT_USED%)"
-        return 0
-    elif [[ "$1" == "gb" ]]; then
-        PERCENT_USED=$(awk "BEGIN {printf \"%.0f\", $USED_MEM_GB / $TOTAL_MEM_GB * 100}")
-        echo "${USED_MEM_GB}GB / ${TOTAL_MEM_GB}GB ($PERCENT_USED%)"
-        return 0
-    else
-        echo "Invalid period ! Please use \"mb\" or \"gb\""
-        return 2
-    fi
+    local cmd="free $UNIT | awk '/Mem:/ {print \$$TYPE}'"
+    eval "$cmd"
 }
 
-function _get_free_mem () {
-    local FREE_MEM_MB=
-    FREE_MEM_MB=$(top -bn 1 | grep "MiB Mem" | cut -d "," -f 2 | sed 's/[a-zA-Z: ]//g; s/+//g')
-    local FREE_MEM_GB=
-    FREE_MEM_GB=$(awk "BEGIN {printf \"%.1f \", $FREE_MEM_MB/1024}")
-
-    local PERCENT_FREE=
-
-    if [[ "$1" == "mb" ]]; then
-        PERCENT_FREE=$(awk "BEGIN {printf \"%.0f\", $FREE_MEM_MB / $TOTAL_MEM_MB * 100}")
-        echo "${FREE_MEM_MB}M/${TOTAL_MEM_MB}M ($PERCENT_FREE%)"
-        return 0
-    elif [[ "$1" == "gb" ]]; then
-        PERCENT_FREE=$(awk "BEGIN {printf \"%.0f\", $FREE_MEM_GB / $TOTAL_MEM_GB * 100}")
-        echo "${FREE_MEM_GB}GB / ${TOTAL_MEM_GB}GB ($PERCENT_FREE%)"
-        return 0
-    else
-        echo "Invalid period ! Please use \"mb\" or \"gb\""
-        return 2
-    fi
-
-}
-
+# TYPE should be: used, available
 TYPE="$1"
-PERIOD="$2"
-
-RETCODE=
-
-if [[ "$TYPE" == "free" ]]; then
-    FREE_MEM_PART=
-    FREE_MEM_PART=$(_get_free_mem $PERIOD)
-    RETCODE="$?"
-    echo "${FREE_MEM_PART}"
-elif [[ "$TYPE" == "used" ]]; then
-    USED_MEM_PART=
-    USED_MEM_PART=$(_get_used_mem $PERIOD)
-    RETCODE="$?"
-    echo "${USED_MEM_PART}"
-else
-    RETCODE=1
-    echo "Invalid type ! Please use \"free\" or \"used\""
+AVAILABLE_TYPES=("used" "available")
+if [[ ! "${AVAILABLE_TYPES[*]}" =~ $TYPE ]]; then
+    echo "Invalid type !"
+    exit 1
 fi
 
-exit "$RETCODE"
+# UNIT should be: b, kb, mb and gb
+UNIT="$2"
+AVAILABLE_UNITS=("b" "kb" "mb" "gb")
+if [[ ! "${AVAILABLE_UNITS[*]}" =~ $UNIT ]]; then
+    echo "Invalid unit !"
+    exit 1
+fi
 
+UNIT_INDICATOR="${UNIT^^}"
+TOTAL_MEM=$(_parse_free_command "total" "$UNIT")
+# TARGET_MEM could be USED_MEM or AVAILABLE_MEM
+TARGET_MEM=$(_parse_free_command "$TYPE" "$UNIT")
+PERCENTAGE=$(awk "BEGIN {printf \"%.0f\", $TARGET_MEM / $TOTAL_MEM * 100}")
+
+echo "${TARGET_MEM}${UNIT_INDICATOR} / ${TOTAL_MEM}${UNIT_INDICATOR} ($PERCENTAGE%)"
